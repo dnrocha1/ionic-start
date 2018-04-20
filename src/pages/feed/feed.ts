5b4ebbe2866168d80203125f216a680c553d96e1
dnrocha1 <daniyel.rocha@ccc.ufcg.edu.br>
2018-04-20 01:39:02 -0300

    Using json

--- src/pages/feed/feed.ts
+++ src/pages/feed/feed.ts
@@ -16,6 +16,15 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 export class FeedPage {
   public nome_usuario:string = "Daniyel Rocha from code";
 
+  public objeto_feed = {
+    titulo: "Daniyel N. Rocha",
+    data: "November 5, 1955",
+    descricao: "Wtf is all of this?!",
+    qtd_likes: 12,
+    qtd_comments: 4,
+    time_comment: "11h ago"
+  }
+
   public somaDoisNumeros() {
     alert(this.nome_usuario);
   }
